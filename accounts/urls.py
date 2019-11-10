8a7263e3db3300806c7ec8a0b818765f3381ec17
congbro <2044smile@naver.com>
2019-11-10 19:09:54 +0900

    logout 완료, LOGOUT_REDIRECT_URL = '/' 로 설정하지 않으면 admin페이지 로그아웃 화면으로 이동

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('signup/', views.UserCreateView.as_view(), name='signup'),
     path('login/', views.login, name='login'),
-    # path('logout/', views.index, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': 'index'}, name='logout'),
 ]
