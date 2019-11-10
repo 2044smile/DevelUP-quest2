1d27192b2c9d095febaa219a1b2c84541d0b457d
congbro <2044smile@naver.com>
2019-11-10 12:44:03 +0900

    First

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,22 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.models import User
 from .models import LoveUser
 
 
 class LoverUserCreationForm(UserCreationForm):
     class Meta:
         model = LoveUser
-        fields = '__all__'
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(LoverUserCreationForm, self).save(commit=False) # 본인의 부모를 호출해서 저장하겠다.
+        if commit:
+            user.save()
+        return user
 
 
 class AccountChangeForm(UserChangeForm):
     class Meta:
         model = LoveUser
         fields = UserChangeForm.Meta.fields
-
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .forms import UserForm
+from .forms import LoverUserCreationForm
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 
 class UserCreateView(CreateView):
-    form_class = UserForm
+    form_class = LoverUserCreationForm
     template_name = 'accounts/signup.html'
-    success_url = "/"
+    success_url = reverse_lazy('/')
